61d8776bd21cb6f03cc2125b9f4453590b47f9f9
efrain <efarinva98@gmail.com>
2019-04-03 16:57:31 -0400

    done with THIS lol

--- assignments/this.js
+++ assignments/this.js
@@ -27,8 +27,27 @@ console.log(human.info());
 // Principle 3
 
 // code example for New Binding
+function Person (name,age) {
+  this.name = name;
+  this.age = age;
+}
 
+Person.prototype.talk = function(){
+  console.log(`say ${this.name} is ${this.age}`);
+}
+
+const efrain = new Person ("efrain","20");
+
+console.log(efrain.talk());
 
 // Principle 4
 
 // code example for Explicit Binding
+
+function info(name,age){
+  return `${name} is ${age} but ${this.name} is ${this.age}`;
+}
+
+const attributes = ["mark", "30"]
+
+console.log(info.call(human, ...attributes));
